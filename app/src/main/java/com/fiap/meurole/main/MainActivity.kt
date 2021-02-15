910cb0b516cf4631a1e11e8cfcc9f54b5390124b
Helton Isac <helton.isac@gmail.com>
2021-02-15 08:33:09 -0300

    Creating a MainActivity

---
+++ app/src/main/java/com/fiap/meurole/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fiap.meurole.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fiap.meurole.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
