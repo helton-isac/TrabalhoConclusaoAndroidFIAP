321838aa2ef519242b07a23b23d57cb824a8fb08
Lyan Masterson Pereira Miguel <lyan_master@hotmail.com>
2021-03-08 23:07:48 -0300

    Add user location.

--- app/src/main/java/com/fiap/meurole/map/MapFragment.kt
+++ app/src/main/java/com/fiap/meurole/map/MapFragment.kt
@@ -1,14 +1,24 @@
 package com.fiap.meurole.map
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import com.fiap.meurole.R
 import com.fiap.meurole.base.BaseFragment
+import com.fiap.meurole.base.auth.NAVIGATION_KEY
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -27,7 +37,15 @@ class MapFragment : BaseFragment(), OnMapReadyCallback {
     private lateinit var etSearch: EditText
     private lateinit var btSearch: Button
 
+    private var permissionDenied = false
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     private val mapViewModel: MapViewModel by viewModel()
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,31 +54,47 @@ class MapFragment : BaseFragment(), OnMapReadyCallback {
         setUpView(view)
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (permissionDenied) {
+            showMessage("Map permission not granted")
+            permissionDenied = false
+        }
+    }
+
     private fun setUpView(view: View) {
         val mapFragment = childFragmentManager.findFragmentById(R.id.viewMap) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
         etSearch = view.findViewById(R.id.etMapSearch)
         btSearch = view.findViewById(R.id.btMapSearch)
         btSearch.setOnClickListener {
-//            val location = etSearch.text.toString()
-//            var addressList: List<Address>? = null
-//
-//            if (location.isNotBlank()) {
-//                val geocoder = Geocoder(this)
-//                addressList = geocoder.getFromLocationName(location, 1)
-//                val address: Address = addressList!![0]
-//                val latLng = LatLng(address.getLatitude(), address.getLongitude())
-//                mMap.addMarker(MarkerOptions().position(latLng).title(location))
-//                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-//            }
+            val location = etSearch.text.toString()
+
+            if (location.isNotBlank()) {
+                val addressList = Geocoder(requireContext()).getFromLocationName(location, 1)
+                if (addressList != null && addressList.count() > 0) {
+                    val address: Address = addressList[0]
+                    val latLong = LatLng(address.getLatitude(), address.getLongitude())
+                    // TODO: Encontrado o local fazer requisição para encontrar os roteiros proximos da camera LatLong
+                    mMap.addMarker(MarkerOptions().position(latLong).title(location))
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLong))
+                } else {
+                    showMessage("Location not found.")
+                }
+            }
         }
     }
 
     private fun registerBackPressedAction() {
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                activity?.finish()
+                findNavController().navigate(
+                    R.id.homeFragment, bundleOf(
+                        NAVIGATION_KEY to findNavController().currentDestination?.id
+                    )
+                )
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
@@ -68,13 +102,33 @@ class MapFragment : BaseFragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        enableMyLocation()
+    }
 
-        val pozale = LatLng(-22.226999, -45.937727)
-        mMap.addMarker(
-            MarkerOptions()
-                .position(pozale)
-                .title("Pouso Alegre - MG")
-        )
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(pozale))
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when(requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation()
+                } else {
+                    permissionDenied = true
+                }
+            }
+        }
+    }
+
+    private fun enableMyLocation() {
+        if (!::mMap.isInitialized) return
+
+        if (ContextCompat.checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestPermissions(Array<String>(1, {ACCESS_FINE_LOCATION}), LOCATION_PERMISSION_REQUEST_CODE)
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude, it.longitude), 15f))
+        }
     }
 }
