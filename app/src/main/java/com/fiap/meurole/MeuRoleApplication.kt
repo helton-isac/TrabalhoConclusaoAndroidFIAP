a3da0e9fbcf91533340addfdbf3524b72f144398
Lyan Masterson Pereira Miguel <lyan_master@hotmail.com>
2021-03-18 18:00:39 -0300

    Add map selection for point of interest

--- app/src/main/java/com/fiap/meurole/MeuRoleApplication.kt
+++ app/src/main/java/com/fiap/meurole/MeuRoleApplication.kt
@@ -3,9 +3,11 @@ package com.fiap.meurole
 import android.app.Application
 import com.fiap.data.di.dataModules
 import com.fiap.meurole.di.presentationModule
+import com.google.android.libraries.places.api.Places
 import com.hitg.domain.di.domainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import java.util.*
 
 class MeuRoleApplication : Application() {
     override fun onCreate() {
@@ -14,5 +16,7 @@ class MeuRoleApplication : Application() {
             androidContext(this@MeuRoleApplication)
             modules(domainModule + dataModules + listOf(presentationModule))
         }
+
+        Places.initialize(this@MeuRoleApplication, resources.getString(R.string.google_maps_key))
     }
 }
