aeb36b4af6b87e2f4cf67e60d2d2d61cd2412a93
Helton Isac <helton.isac@gmail.com>
2021-03-21 16:19:47 -0300

    fix heritage of fragments

--- app/src/main/java/com/fiap/meurole/roadmapList/RoadmapListFragment.kt
+++ app/src/main/java/com/fiap/meurole/roadmapList/RoadmapListFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.fiap.meurole.R
-import com.fiap.meurole.base.BaseFragment
+import com.fiap.meurole.base.auth.BaseAuthFragment
 import com.fiap.meurole.utils.DialogUtils
 import com.hitg.domain.entity.RequestState
 import com.hitg.domain.entity.Roadmap
@@ -12,7 +12,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @ExperimentalCoroutinesApi
-class RoadmapListFragment : BaseFragment() {
+class RoadmapListFragment : BaseAuthFragment() {
 
     override val layout = R.layout.roadmap_list_fragment
 
